2726e3444d64bc57c91a30ad73acee782d135aa5
Runneker74 <122167596+Runneker74@users.noreply.github.com>
2023-09-07 02:02:41 +0000

    codigo semaforo

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,34 @@
 //import react into the bundle
-import React from "react";
 import ReactDOM from "react-dom";
+import React, { useState } from 'react';
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+
+
+function Semaforo() {
+    const [seleccion, setSeleccion] = useState(null);
+  
+    const handleClick = (luz) => {
+      setSeleccion(luz);
+      if( seleccion === 'verde')  {
+        handleClick('verde');
+      } else {
+        handleClick('gris');
+      }
+    };
+
+    return (
+        
+        <div className="Semaforo">
+            <button onClick={Semaforo}>colorVerde</button>
+        </div>
+
+        
+        );
+
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+            ReactDOM.render(<Semaforo />, document.querySelector("#app"));
